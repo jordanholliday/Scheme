98b54ff12cd2b8ea254b9c04c18d45a80d437ee9
Jordan Holliday <jordanholliday@gmail.com>
2016-04-04 13:34:20 -0400

    Complete task assignment dropdown styles and breakout lis into subcomponent

--- frontend/components/task_options.jsx
+++ frontend/components/task_options.jsx
@@ -32,12 +32,18 @@ var TaskOptions = React.createClass({
     ApiUtil.fetchTeamUsers();
   },
 
+  componentDidUpdate: function () {
+    if (this.state.assigning) {
+      this.refs.assigneeInput.focus();
+    }
+  },
+
   setStateAssigning: function (e) {
     e.stopPropagation();
     if (this.state.assigning) {
       this.setState({assigning: false});
     } else {
-      this.setState({assigning: true});
+      this.setState({assigning: true, assigneeId: null });
     };
   },
 
@@ -65,7 +71,20 @@ var TaskOptions = React.createClass({
   },
 
   assigneeInputClick: function (e) {
-    e.stopPropagation();
+  },
+
+  teamUserDropdown: function () {
+    var teamUserLis = [];
+    if (this.state.teamUsers) {
+      for (var id in this.state.teamUsers) {
+        teamUserLis.push(<AssigneeDropdownLi teamUser={this.state.teamUsers[id]} />);
+      }
+    }
+    return (
+      <div className="team-user-div">
+        {teamUserLis}
+      </div>
+      );
   },
 
   render: function () {
@@ -73,20 +92,20 @@ var TaskOptions = React.createClass({
     if (this.assigneeName()) {
       currentAssigneeDetail = <div className="group current-assignee">
           <img src={this.assigneeAvatar()} />
-          <label>{this.assigneeName()}</label>
-          <input type="text" value={this.assigneeName()} />
+          <input type="text" ref="assigneeInput" placeholder={this.assigneeName()} />
+          {this.teamUserDropdown()}
         </div>
     } else {
       currentAssigneeDetail = <div className="group current-assignee unassigned">
-            <svg class="" viewBox="0 0 32 32">
+            <svg viewBox="0 0 32 32">
               <path d="M20.073,18.606C22.599,16.669,24,12.995,24,9.412C24,4.214,21.054,0,16,0S8,4.214,8,9.412 c0,3.584,1.401,7.257,3.927,9.194C6.182,20.351,2,25.685,2,32h2c0-6.617,5.383-12,12-12s12,5.383,12,12h2 C30,25.685,25.818,20.351,20.073,18.606z M10,9.412C10,4.292,13.013,2,16,2s6,2.292,6,7.412C22,13.633,19.756,18,16,18 C12.244,18,10,13.633,10,9.412z"></path>
             </svg>
-            <label>Unassigned</label>
             <input
+              ref="assigneeInput"
               type="text"
-              placeholder={this.assigneeName()}
-              autoFocus={this.state.assigning ? true : false}
-              onClick={this.assigneeInputClick} />
+              value={this.assigneeName()}
+              placeholder="Unassigned" />
+              {this.teamUserDropdown()}
         </div>
     }
 
@@ -97,6 +116,16 @@ var TaskOptions = React.createClass({
           </div>
         </section>
       );
+  }
+});
+
+var AssigneeDropdownLi = React.createClass({
+  render: function () {
+    return (<li className="group" >
+      <img src={this.props.teamUser.avatar_url} />
+      <label className="name">{this.props.teamUser.name}</label>
+      <label className="email">{this.props.teamUser.email}</label>
+    </li>);
   }
 });
 
